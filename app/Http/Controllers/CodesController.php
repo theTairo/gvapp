fb9e7b3df5e74653899ae92a6008e66b8743f6e1
theTairo <tiropolska@gmail.com>
2016-06-29 23:06:49 +0200

    validate test

--- app/Http/Controllers/CodesController.php
+++ app/Http/Controllers/CodesController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+
+use App\Code;
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Helpers\functions;
+
 class CodesController extends Controller
 {
-    //
+	public function getCode()
+	{
+		$keyNumber = rand(1, 100);
+		$code = Code::find($keyNumber);
+		return $code;
+	}
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,9 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Mail;
+
 use App\Http\Requests;
 
-class UsersController extends Controller
+use App\User;
+
+use App\Code;
+
+class UserController extends CodesController
 {
-    //
+    public function store(Request $req)
+    {
+		$user = new User;
+		$code = $this->getCode();
+
+		$this->validate($req, [
+			'first_name' => 'required'
+		]);
+
+		$user->first_name = $req->first_name;
+		$user->last_name = $req->last_name;
+		$user->email = $req->email;
+		$user->country_tag = $req->country;
+		$user->code_id = $code->id;
+		
+		Mail::send('emails.test', ['name' => 'Easy'], function ($message)
+		{
+			$message->to('tiropolska@gmail.com', 'Some guy')->subject('Welcome!');
+		});
+    }
 }

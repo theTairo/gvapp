dc3443c9f8a2c32e6a582a1ba91722068fb843d1
theTairo <tiropolska@gmail.com>
2016-07-01 00:02:59 +0200

    database

--- config/database.php
+++ config/database.php
@@ -1,12 +1,5 @@
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-
 return [
     
     /*
@@ -61,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host'      => $host,
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database'  => $database,
-            'username'  => $username,
-            'password'  => $password,
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',

2caeac70e530efb5da17f0e8d990ee9935f2ec58
theTairo <tiropolska@gmail.com>
2016-07-01 12:36:10 +0200

    m

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'host' => env('DB_HOST', '91.185.184.241'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'mccm_gvapp'),
-            'username' => env('DB_USERNAME', 'mccm_admin'),
+            'username' => env('DB_USERNAME', 'mccm'),
             'password' => env('DB_PASSWORD', 'tYAbr6zJfn'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',

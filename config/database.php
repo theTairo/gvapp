b37e6715429289fca95702334f783ea0d02f0a09
theTairo <tiropolska@gmail.com>
2016-07-01 12:33:29 +0200

    db

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '91.185.184.241'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'mccm_gvapp'),
+            'username' => env('DB_USERNAME', 'mccm'),
+            'password' => env('DB_PASSWORD', 'tYAbr6zJfn'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
